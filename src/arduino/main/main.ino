84bf759b6c751e8de48fe53d8a544ebbf03af82c
Joseph Patrick <jpatrick5402@gmail.com>
2026-01-11 10:22:58 -0500

    Web server on hold, adding send_email()

--- src/arduino/main/main.ino
+++ src/arduino/main/main.ino
@@ -1,98 +1,107 @@
 // main.c - source code for APPS system
 // Delveloped by Joseph Patrick
 #include <WiFi.h>
-#include <WebServer.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
-#include <Stepper.h>
 #include <ESP_Mail_Client.h>
+#include <Stepper.h>
+#include <HX711.h>
 
-const String times[] = {"07:00", "18:00"};
+#define WIFI_SSID "Your Wi-Fi Name"
+#define WIFI_PASSWORD "Your Wi-Fi Password"
 
-const int pump_pin = 32;
+#define EMAIL_FROM "Your email"
+#define EMAIL_TO "Your email or elsewhere"
+#define EMAIL_PASSWORD "Your email application password"
+https://support.google.com/accounts/answer/185833?hl=en
+#define EMAIL_SMTP_PORT YOUR_EMAIL_PROVIDERS_SMTP_PORT
+#define EMAIL_SMTP_HOST "YOUR.EMAIL.PROVIDERS.SMTP.HOST"
 
-Stepper stepper(2048, 13,12,14,27); // stepper step count & pins
+#define STP_1 13
+#define STP_2 12
+#define STP_3 14
+#define STP_4 27
+#define PUMP_PIN 26
 
-const char* ssid = "";
-const char* wifi_password = "";
+const String times[2] = {"07:00", "18:00"}; // times to output provisions
 
-const char* from_email = "";
-const char* email_password = "";
-const char* to_email = "";
+Stepper stepper(2048, STP_1, STP_2, STP_3, STP_4); // stepper step count & pins
 
-WebServer server(80);
+SMTPSession smtp;
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "north-america.pool.ntp.org", -18000, 60000);
 
 void setup_wifi() { // connect to WiFi
-  WiFi.begin(ssid, wifi_password);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED) { // Wait for connection
     delay(500);
     Serial.print(".");
   }
   Serial.print("\n");
-  Serial.print("\nConnected to the Wi-Fi network: ");
+  Serial.print("Connected to the Wi-Fi network: ");
   Serial.println(WiFi.SSID());
   Serial.print("Local ESP32 IP: ");
   Serial.println(WiFi.localIP());
 }
 
-void setup_email() { // send an email to admin if error or empty
-
-}
-
-void render_root() {
-  char* content = "<html><p>Hello There</p></html>";
-  server.send(200, "text/html", content);
-}
-
-void setup_web_server() {
-  server.on("/", render_root);
-  Serial.println("Starting web server");
-  server.begin();
-  Serial.println("Web server initialized");
-}
-
 void setup() {
   Serial.begin(115200);
-  Serial.println("\n\n---------------------------------------------");
+  Serial.println(F("\n\n---------------------------------------------"));
   setup_wifi();
-  setup_email();
-  setup_web_server();
   timeClient.begin();
   stepper.setSpeed(10);
-  pinMode(pump_pin, OUTPUT);
+  pinMode(PUMP_PIN, OUTPUT);
+  timeClient.update();
+}
+
+void send_email(String text) {
+  ESP_Mail_Session session;
+  session.server.host_name = EMAIL_SMTP_HOST;
+  session.server.port = EMAIL_SMTP_PORT;
+  session.login.email = EMAIL_FROM;
+  session.login.password = EMAIL_PASSWORD;
+  SMTP_Message message;
+  message.sender.email = EMAIL_FROM;
+  message.subject = "APPS Notification";
+  message.addRecipient("", EMAIL_TO);
+  message.html.content = text;
+  smtp.connect(&session);
+  MailClient.sendMail(&smtp, &message);
 }
 
 void loop() {
-  server.handleClient();
-  timeClient.update(); // update and store the current time
   String time = timeClient.getFormattedTime().substring(0,5);
   Serial.println(time);
-
-  bool isActivationTime = false;
+  delay(5000);
+  digitalWrite(PUMP_PIN, HIGH);
+  delay(2000);
+  digitalWrite(PUMP_PIN, LOW);
+  delay(2000);
   for (String t : times) { // loop through times and check if any of them are now
-    if (t == time) isActivationTime = true;
-  }
+    if (t == time) {
+      Serial.println("time triggered");
+      // while (food is below threshold) {
+      //   stepper.step(512);
+      //   re-measure
+      // }
+      // if (food is empty) {
+      //   send_email("Food is empty");
+      // }
+      //
+      // while (water is below threshold) {
+      //   digitalWrite(pump_pin, HIGH);
+      //   re-measure;
+      // }
+      // digitalWrite(pump_pin, LOW);
+      // if (water is empty) {
+      //   send_email("Water is empty");
+      // }
 
-  if (isActivationTime) {
-    Serial.println("time triggered");
-    // while (food is below threshold) {
-    //   stepper.step(512);
-    //   re-measure
-    // }
-    //
-    // while (water is below threshold) {
-    //   digitalWrite(pump_pin, HIGH);
-    //   re-measure;
-    // }
-    // digitalWrite(pump_pin, LOW);
-
-    delay(60000); // wait for the minute to be over so it doesn't re-trigger
-  } else {
-    delay(3000); // wait 3 seconds to check if we're now in trigger minute
-                 // 3 seconds because of web server handleClient()
+      delay(60000); // wait for the minute to be over so it doesn't re-trigger
+      timeClient.update(); // update to current time
+    }
   }
+  delay(1000);
 }
